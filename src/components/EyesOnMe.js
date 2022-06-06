9052b20efbc7215e84ada8858e81e9db984f7310
youbaidjis <youba.etud@gmail.com>
2022-06-05 22:10:45 -0700

    Completed assignment

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,10 @@
-// Code EyesOnMe Component Here
+import React from "react";
+
+function EyesOnMe(){
+    return (
+        <div>
+            <button onFocus={()=>{console.log("Good!")}} onBlur={()=>{console.log("Hey! Eyes on me!")}}>Eyes on me</button>
+        </div>
+    )
+}
+export default EyesOnMe;
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,10 @@
-// Code Keypad Component Here
+import React from "react";
+
+function Keypad(){
+    return (
+        <div>
+            <input type="password" onChange={()=>{console.log("Entering password...")}}></input>
+        </div>
+    )
+}
+export default Keypad;
